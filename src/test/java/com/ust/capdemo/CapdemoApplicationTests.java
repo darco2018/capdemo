bd3f7ad6218d1e3f16ad2dd090e10c4903f307dc
darco2018 <newsecond@outlook.com>
2022-08-04 12:48:03 +0200

    Initial commit

---
+++ src/test/java/com/ust/capdemo/CapdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.capdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
